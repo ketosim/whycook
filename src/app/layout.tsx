7799d0d71815b641f31fb7cdbd2fb096c5baa340
Sim <yhu348@gmail.com>
2025-01-03 12:42:07 +1100

    Update: Changed recipe title font to Twin Peaks style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 // app/layout.tsx
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { Red_Hat_Display } from 'next/font/google'
+import { Antonio } from 'next/font/google'
 
 // Initialize fonts before the component
-const inter = Inter({ 
-  subsets: ['latin'] 
+const inter = Inter({
+  subsets: ['latin']
 })
 
-const redHat = Red_Hat_Display({
+const antonio = Antonio({
   subsets: ['latin'],
   weight: ['700'],
   display: 'swap',
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${redHat.className}`}>
+      <body className={`${inter.className} ${antonio.className}`}>
         {children}
       </body>
     </html>
